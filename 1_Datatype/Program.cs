6c6ea107b593666d877e4d99ddaea9975bb03956
Harin <gunwoo8873@outlook.kr>
2026-03-22 17:28:44 +0900

    Add user input helper menu example

--- 1_Datatype/Program.cs
+++ 1_Datatype/Program.cs
@@ -4,16 +4,39 @@ namespace _1_Datatype;
 
 public class Logic
 {
-  static void Main(string[] args)
+  private static void Menu()
+  {
+    Console.WriteLine($"""
+    1. int (ex 1)
+    2. double (ex 1.5)
+    3. float (ex 1.5f)
+    4. bool (ex true)
+    5. info (Docs)
+    """);
+  }
+
+  private static void PrintInfo()
+  {
+    Console.WriteLine($"""
+    Datatypes:
+    - int    Length : {int.MinValue} - {int.MaxValue}
+    - double Length : {double.MinValue} - {double.MaxValue}
+    - float  Length : {float.MinValue} - {float.MaxValue}
+    - bool   Length : {bool.FalseString} - {bool.TrueString}
+    """);
+  }
+
+  static void Main()
   {
     // User input of get value from console
+    Menu();
     Console.Write("Enter a value: ");
     var getValue = Console.ReadLine();
 
     // Get the type of the value entered by the user and print it to the console
     switch (getValue)
     {
-      ///
+      // 
       case string s when int.TryParse(s, out _):
         Console.WriteLine($"Value: {s}, Type: {typeof(int)}");
         break;
@@ -26,14 +49,8 @@ public class Logic
       case string s when bool.TryParse(s, out _):
         Console.WriteLine($"Value: {s}, Type: {typeof(bool)}");
         break;
-      case "Help":
-        Console.WriteLine($"""
-        Datatypes:
-        - int    Length : {int.MinValue} - {int.MaxValue}
-        - double Length : {double.MinValue} - {double.MaxValue}
-        - float  Length : {float.MinValue} - {float.MaxValue}
-        - bool   Length : {bool.FalseString} - {bool.TrueString}
-        """);
+      case "Docs" or "docs" or "Info" or "info":
+        PrintInfo();
         break;
       default:
         Console.WriteLine($"Value: {getValue}, Type: {typeof(string)}");
