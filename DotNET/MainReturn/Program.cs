ac4cf2eb6670d3f46aa5b846a5b183341a47f7b1
geonwoo <geonwoo8873@outlook.kr>
2026-04-07 02:39:31 +0900

    Add initial project files for MainReturn application

---
+++ DotNET/MainReturn/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ReturnValue;
+
+class Program
+{
+    struct TypeField
+    {
+        public string s;
+        public int n;
+    }
+
+    public string Str(string s) => s;
+    public int Num(int n) => n;
+
+    static void Main()
+    {
+        TypeField tf = new TypeField();
+        
+        tf.s = "String";
+        Console.WriteLine($"tf.s: {tf.s}");
+        tf.n = 100;
+        Console.WriteLine($"tf.n: {tf.n}");
+
+        Program p = new Program();
+        
+        string s = p.Str("String");
+        Console.WriteLine($"s: {s}");
+        int n = p.Num(100);
+        Console.WriteLine($"n: {n}");
+    }
+}
