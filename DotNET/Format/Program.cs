15869823738214266428bc98f2d61f029e74a589
geonwoo <geonwoo8873@outlook.kr>
2026-04-08 12:04:27 +0900

    Add Program.cs and Recode.csproj files; implement temperature data processing with readonly record struct

---
+++ DotNET/Format/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+class Program
+{
+    private const string Line = "------------------------------";
+
+    string ReturnValue(string s)
+    {
+        return s;
+    }
+
+    static void UpperFormat()
+    {
+        Program program = new Program();
+        string result = program.ReturnValue("This is a string.");
+
+        Console.WriteLine($"""
+        {Line}
+        Before : {result}
+        After Upper : {$"[{result.ToUpper()}]"}
+        After Lower : {$"[{result.ToLower()}]"}
+        """);
+    }
+
+    static void TrimFormat()
+    {
+        Program program = new Program();
+        string result = program.ReturnValue("      The Trim method string.     ");
+
+        Console.WriteLine($"""
+        {Line}
+        Before : {result}
+        After Normal Trim : {$"[{result.Trim()}]"}
+        After Trim Start  : {$"[{result.TrimStart()}]"}
+        After Trim End    : {$"[{result.TrimEnd()}]"}
+        """);
+    }
+    
+    static void ReplaceFormat()
+    {
+        Program program = new Program();
+        string result = program.ReturnValue("The Replace method string.");
+
+        Console.WriteLine($"""
+        {Line}
+        Before : {result}
+        After Replace : {$"[{result.Replace("Replace", "Replaced")}]"}
+        """);
+    }
+
+    static void SearchFormat()
+    {
+        Program program = new Program();
+        string result = program.ReturnValue("The Search method string.");
+        Console.WriteLine($"""
+        {Line}
+        Before : {result}
+        Get Search : {$"[{result.Contains("Search")}]"}
+        Get Search : {$"[{result.Contains("search")}]"}
+        """);
+    }
+
+    static void Main()
+    {
+        UpperFormat();
+        TrimFormat();
+        ReplaceFormat();
+        SearchFormat();
+    }
+}
--- DotNET/MainReturn/Program.cs
+++ DotNET/MainReturn/Program.cs
@@ -6,7 +6,6 @@ namespace ReturnValue;
 // 1. Main는 `static`이 아닌 멤버는 사용할 수 없다.
 // 2. 액세스 한정자를 소유할 수 있다. <Public, Private, Internal, ...etc>
 // 3. `void int Task`나 `Task<T>`을 반환할 수 있다.
-
 class Program
 {
     static string ReturnValue(string s)
@@ -14,9 +13,11 @@ class Program
         return s;
     }
 
-    // 
+    // `Task` 또는 `Task<T>`는 타입의 결과를 반환하는 작업
+    // 비동기 메서드에서 사용되며, `async`와 `await` 키워드를 사용하여 비동기 작업이 완료될 때까지 대기 후 처리
     static async Task<string> AsyncReturnValue(string s)
     {
+        // `Task.Delay(n)`는 지정된 `n` 시간동안 비동기적으로 대기하는 작업을 생성
         await Task.Delay(100);
         return s;
     }
@@ -26,9 +27,45 @@ class Program
         return await AsyncReturnValue("Hello World");
     }
 
+    static void TypeChangedValue()
+    {   
+        // `Parse`는 문자열을 해당 타입으로 변환하는
+        Console.WriteLine($"Type Changed Value of Parse: {int.Parse("123")}");
+        
+        // `Convert.ToInt32`는 다양한 타입을 정수로 변환하는
+        Console.WriteLine($"Type Changed Value of Convert: {Convert.ToInt32("123")}");
+    }
+
     static async Task Main(string[] args)
     {
         Console.WriteLine(ReturnValue("Hello World"));
         Console.WriteLine(await OuputAsyncValue());
+    
+        TypeChangedValue();
+        Example.ExampleMain();
+    }
+}
+
+public class Example
+{
+    public string GetString(int ID)
+    {
+        if (ID < names.Length)
+        {
+            return names[ID];
+        }
+        else
+        {
+            return "ID is out of range.";
+        }
+    }
+
+    private string[] names = { "Alice", "Bob", "Charlie" };
+
+    public static void ExampleMain()
+    {
+        Example example = new Example();
+        Console.WriteLine(example.GetString(1)); // Output: Bob
+        Console.WriteLine(example.GetString(5)); // Output: ID is out of range.
     }
 }
