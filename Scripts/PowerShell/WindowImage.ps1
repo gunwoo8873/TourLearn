226fbf4cae9904b39f4930d880e761cbbf1552c1
geonwoo <geonwoo8873@outlook.kr>
2026-04-08 20:20:43 +0900

    Window health recovery cmd

---
+++ Scripts/PowerShell/WindowImage.ps1
@@ -0,0 +1,11 @@
+Writle-Host " Checking for Windows image health"
+DISM /Online /Cleanup-Image /CheckHealth
+
+Write-Host " Scanning for Windows image health issues"
+DISM /Online /Cleanup-Image /ScanHealth
+
+Write-Host " Restoring Windows image health"
+DISM /Online /Cleanup-Image /RestoreHealth
+
+Write-Host " Running System File Checker to repair any corrupted system files"
+sfc /scannow
